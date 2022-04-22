5aa3e21ae9f2b5e28ae673d65dfb8eba89e57e2a
davidwdcn24 <davidwdcn24@gmail.com>
2022-04-22 18:25:18 -0500

    Completar funcionalidad de login.

---
+++ mern2022/src/bll/data.js
@@ -0,0 +1,8 @@
+let users = [
+    { email: "santiago", password: "alulema" }, 
+    { email: "pablo", password: "123456" }
+]
+
+export function getUsers(){
+    return users;
+}
--- mern2022/src/components/login.jsx
+++ mern2022/src/components/login.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from "react";
+import { getUsers } from "../bll/data";
 
 const Login = () => {
-    const[password, setPassword] = useState("")
-    const[email, setEmail] = useState("")
-    const[msnError, setMsnError] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [msnError, setMsnError] = useState("")
 
-    const users = [{email: "santiago", password: "alulema"}
-                    ,{email:"pablo", password:"123456"}]
+    // Obtiene los usuarios del repositorio.
+    const users = getUsers();
 
-    const validateUser =() =>{
+    const validateUser = () => {
         const user = users.find(x => x.email === email && x.password === password);
-        if (user === undefined){
+        if (user === undefined) {
             setMsnError("Usuario o password invalido")
-        } else{
+        } else {
             setMsnError(`Usuario ${user.email} encontrado`)
         }
     }
-  return (
-    <div className="container">
-        <h3>LOGIN</h3>
-        <div className="input-group flex-nowrap mb-3">
-            <span className="input-group-text" id="email">@</span>
-            <input type="email" 
-                   className="form-control" 
-                   placeholder="email" 
-                   aria-label="email" 
-                   onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="input-group flex-nowrap">
-            <span className="input-group-text" 
+
+    return (
+        <div className="container">
+            <h3>LOGIN</h3>
+            {msnError !== null && msnError.length > 0 && (
+                <div className='alert alert-warning mt-3'>{msnError}</div>
+            )}
+            <div className="input-group flex-nowrap mb-3">
+                <span className="input-group-text" id="email">@</span>
+                <input type="email"
+                    className="form-control"
+                    placeholder="email"
+                    aria-label="email"
+                    onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className="input-group flex-nowrap">
+                <span className="input-group-text"
                     id="password">Ps</span>
-            <br></br>
-            <input type="password" 
-                    className="form-control" 
-                    placeholder="password" 
+                <br></br>
+                <input type="password"
+                    className="form-control"
+                    placeholder="password"
                     aria-label="password"
                     onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+                <button type="button" className="btn btn-primary btn-lg" onClick={validateUser}>Primary</button>
+            </div>
         </div>
-        <div  className="d-grid gap-2 mt-3">
-            <button type="button" className="btn btn-primary btn-lg" onClick={validateUser}>Primary</button>
-        </div>
-        <div className="alert alert-warning mt-3" role="alert">
-            {msnError}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Login
